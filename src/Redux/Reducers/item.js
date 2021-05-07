1ac22b42c33e8596c61bdf06ecc06831ae5a5a15
kbjude <kbjude@gmail.com>
2021-05-08 00:35:40 +0300

    WIP user reducer

---
+++ src/Redux/Reducers/item.js
@@ -0,0 +1,39 @@
+import * as types from "../actions/actionTypes";
+
+const initialState = {
+  isLoading: true,
+  error: "",
+  name: "",
+  descrption: "",
+  created_at: "",
+  updated_at: ""
+};
+
+export default function itemReducer(state = initialState, action) {
+  switch (action.type) {
+    case types.GET_ITEMS:
+      return { ...state, isLoading: true };
+    case types.GET_ITEMS_ERROR:
+      return { ...state, error: action.error, isLoading: false };
+    case types.GET_ITEMS_SUCCESS:
+      return {
+        ...state,
+        items: action.items,
+        isLoading: false
+      };
+    case types.CREATE_ITEM:
+      return { ...state, isLoading: true };
+    case types.CREATE_ITEMS_ERROR:
+      return { ...state, error: action.error, isLoading: false };
+    case types.CREATE_ITEMS_SUCCESS:
+      return {
+        ...state,
+        response: action.response,
+        isLoading: false,
+        showModal: false,
+        error: ""
+      };
+    default:
+      return state;
+  }
+}
---
+++ src/Redux/Reducers/user.js
@@ -0,0 +1,37 @@
+
+import * as types from "../actions/actionTypes";
+
+const initialState = {
+  isLoading: true,
+  error: "",
+  name: "",
+};
+
+export default function itemDetailsReducer(state = initialState, action) {
+  switch (action.type) {
+    case types.GET_ITEMDETAILS:
+      return { ...state, isLoading: true };
+    case types.GET_ITEMDETAILS_ERROR:
+      return { ...state, error: action.error, isLoading: false };
+    case types.GET_ITEMDETAILS_SUCCESS:
+      return {
+        ...state,
+        itemdetails: action.itemdetails,
+        isLoading: false
+      };
+    case types.CREATE_ITEMDETAIL:
+      return { ...state, isLoading: true };
+    case types.CREATE_ITEMDETAILS_ERROR:
+      return { ...state, error: action.error, isLoading: false };
+    case types.CREATE_ITEMDETAILS_SUCCESS:
+      return {
+        ...state,
+        response: action.response,
+        isLoading: false,
+        showModal: false,
+        error: ""
+      };
+    default:
+      return state;
+  }
+}
