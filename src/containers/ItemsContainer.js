38e65c58ad340b89f7d4171e3e5705a48c6eb753
kbjude <kbjude@gmail.com>
2021-05-15 08:34:16 +0300

    items list complete

--- src/containers/ItemsContainer.js
+++ src/containers/ItemsContainer.js
@@ -1,34 +1,32 @@
 import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
-import styled from 'styled-components';
-import store from '../Redux/store';
+// import styled from 'styled-components';
+// import store from '../Redux/store';
 import fetchItems from '../Api/itemApi';
 import Items from '../components/Items';
 
-const ItemsContainer = () => {
-  const { item, offset } = useSelctor((state) => state);
+function ItemsContainer() {
+  const { items } = useSelector((state) => state);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchItems());
   }, []);
- 
-  useEffect(() => {
-    if (store.getState().item.categories.length === 0) {
-      dispatch(fetchItems());
-    }
-  }, [offset]);
 
-
-    <div>
-      <Items
-        id={id}
-        name={name}
-        description={description}
-      />
-    </div>;
-};
+  return (
+    <>
+      {items && items.map((item) => (
+        <Items
+          key={item.id}
+          id={item.id}
+          name={item.name}
+          description={item.description}
+        />
+      ))}
+    </>
+  );
+}
 
 ItemsContainer.propTypes = {
 
