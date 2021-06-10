e401ae74df20e7ef2e09f833788e12e2db4972d6
kbjude <kbjude@gmail.com>
2021-06-11 02:01:59 +0300

    WIP actions

---
+++ src/Redux/actions/auth.js
@@ -0,0 +1,10 @@
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+  SET_MESSAGE,
+} from './types';
+
+import AuthService from "../services/auth.service";
