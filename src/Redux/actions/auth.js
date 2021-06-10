07a643fdaf8ab82ee05e7b6fb236a580150c3e3d
kbjude <kbjude@gmail.com>
2021-06-11 02:16:18 +0300

    WIP auth action

--- src/Redux/actions/auth.js
+++ src/Redux/actions/auth.js
@@ -7,4 +7,41 @@ import {
   SET_MESSAGE,
 } from './types';
 
-import AuthService from "../services/auth.service";
+import AuthService from '../../Api/auth/auth.service';
+
+export const register = (
+  username,
+  email,
+  password,
+) => (dispatch) => AuthService.register(username, email, password).then(
+  (response) => {
+    dispatch({
+      type: REGISTER_SUCCESS,
+    });
+
+    dispatch({
+      type: SET_MESSAGE,
+      payload: response.data.message,
+    });
+
+    return Promise.resolve();
+  },
+  (error) => {
+    const message = (error.response
+            && error.response.data
+            && error.response.data.message)
+          || error.message
+          || error.toString();
+
+    dispatch({
+      type: REGISTER_FAIL,
+    });
+
+    dispatch({
+      type: SET_MESSAGE,
+      payload: message,
+    });
+
+    return Promise.reject();
+  },
+);
