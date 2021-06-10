91e2cf78c4585de02518d36ac08c7f7725206fb4
kbjude <kbjude@gmail.com>
2021-06-11 01:45:15 +0300

    user.service

--- src/Api/auth/user.service.js
+++ src/Api/auth/user.service.js
@@ -1,9 +1,19 @@
 import axios from 'axios';
 import authHeader from './authHeader';
 
-
 const API_URL = 'http://localhost:3000/api';
 
-const getPublicContent = () => {
-    retu
-}
+const getPublicContent = () => axios.get(`${API_URL}all`);
+
+const getUserBoard = () => axios.get(`${API_URL}user`, { headers: authHeader() });
+
+const getModeratorBoard = () => axios.get(`${API_URL}mod`, { headers: authHeader() });
+
+const getAdminBoard = () => axios.get(`${API_URL}admin`, { headers: authHeader() });
+
+export default {
+  getPublicContent,
+  getUserBoard,
+  getModeratorBoard,
+  getAdminBoard,
+};
