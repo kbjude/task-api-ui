2d067d652a277c849e9d3d43c204ffd019277d3e
kbjude <kbjude@gmail.com>
2021-06-12 00:15:22 +0300

    remaining reducers

--- src/Redux/Reducers/index.js
+++ src/Redux/Reducers/index.js
@@ -2,11 +2,15 @@ import { combineReducers } from 'redux';
 import users from './user';
 import { itemReducer } from './item';
 import itemDetailReducer from './itemDetails';
+import message from './message';
+import auth from './auth';
 
 const rootReducer = combineReducers({
   users,
   itemReducer,
   itemDetailReducer,
+  auth,
+  message,
 });
 
 export default rootReducer;
